42ea5cf8fb3d7d2bca3650476f14dc31c0009ce3
opszalek <dawidc4@hotmail.com>
2023-08-17 00:53:12 +0200

    Begining of main file

--- main.py
+++ main.py
@@ -7,54 +7,76 @@ from stable_baselines3.common.callbacks import EvalCallback
 from stable_baselines3.common import results_plotter
 from stable_baselines3.common.results_plotter import load_results, ts2xy, plot_results
 from stable_baselines3.common.monitor import Monitor
-
+from stable_baselines3.common.env_util import make_vec_env
+import csv
+import os
 import matplotlib.pyplot as plt
-import time
-from queue import Queue
+
+environments = ["Hopper-v4", "Swimmer-v4", "Cartpole-v1", "Acrobot-v1"]
+model_names = ["PPO", "DDPG", "A2C", "SAC"]
+
+def load_hyperparameters(environment_name, algoritm_name):
+    hyperparameters = {}
+
+    with open('hyperparameters/' + algoritm_name + '_hyperparameters' + '.csv', mode='r') as csv_file:
+        reader = csv.reader(csv_file)
+        next(reader)
+        for row in reader:
+            env_name, param_key, param_value = row
+            if env_name == environment_name:
+                hyperparameters[param_key] = param_value
+
+    return hyperparameters
+
+def ppo_create_model(environment_name, parameters):
+    time_steps = int(float(parameters.pop("total_timesteps")))
+    n_envs = parameters.pop("n_envs")
+    print(parameters)
+    vec_env = make_vec_env(environment_name, n_envs=int(n_envs))
+    model = PPO(env=vec_env, verbose=1, **parameters)
+    model.learn(total_timesteps=time_steps)  # Uczenie modelu
+    model.save('models/' + environment_name + "_model")  # Zapisanie modelu
+    vec_env.close()
+
+def ppo_algoritm(environments):
 
 
 
+    for environment_name in environments:
+        parameters = load_hyperparameters(environment_name, "PPO")
+        time_steps = int(float(parameters.pop("total_timesteps")))
+        n_envs = parameters.pop("n_envs")
+        print(parameters)
+        vec_env = make_vec_env(environment_name, n_envs=int(n_envs))
+        model = PPO(env=vec_env,verbose=1,**parameters)
+        model.learn(total_timesteps=time_steps)  # Uczenie modelu
+        model.save('models/' + environment_name + "_model")  # Zapisanie modelu
+        vec_env.close()
+
+def create_model(environment_name,model_name):
+
+    pass
+def test_models():
+    for environment_name in environments:
+        for model in model_names:
+            # print(model,environment_name)
+            if not os.path.exists('models/'+model+'/'+environment_name+'_model.zip'):
+                pass
 
 
+def main():
+    # ppo_models(environments)##
+    # ddpg_models(environments)
+    # a2c_models(environments) ##
+    # sac_models(environments)
+    test_models()
+    params = load_hyperparameters("Swimmer-v4", "PPO")
+    print(params)
+
+    pass
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+main()
 
 ############Function to create a plot in real time
 
@@ -68,12 +90,6 @@ from queue import Queue
 #
 # plot_results([log_dir], timesteps, results_plotter.X_TIMESTEPS, "CartPole-v1")
 # plt.show()
-
-
-
-
-
-
 
 
 #
