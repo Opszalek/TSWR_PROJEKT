22550d3d8fa8253957c39681dcfd5a4a2f827077
opszalek <dawidc4@hotmail.com>
2023-08-18 23:44:19 +0200

    Everything seems to work properly

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 import gymnasium as gym
 import multiprocessing
 import threading
-from stable_baselines3 import PPO
-from stable_baselines3 import A2C
-from stable_baselines3 import DDPG
-from stable_baselines3 import DQN
+from stable_baselines3 import PPO, A2C, DDPG, DQN
 from stable_baselines3.common.vec_env import DummyVecEnv
 from stable_baselines3.common.callbacks import EvalCallback
 from stable_baselines3.common import results_plotter
@@ -15,37 +12,41 @@ import csv
 import os
 import matplotlib.pyplot as plt
 
-environments = ["CartPole-v1"]#["Hopper-v4", "Swimmer-v4", "Cartpole-v1", "Acrobot-v1"]
-model_names = ["PPO", "DDPG", "A2C", "DQN"]
+environments = ["Hopper-v4", "Swimmer-v4", "CartPole-v1", "Acrobot-v1"]
+model_names = ["DDPG", "DQN", "PPO", "A2C"]
 
 
 def load_hyperparameters(environment_name, algorithm_name):
     hyperparameters = {}
-
+    if not os.path.exists('hyperparameters/' + algorithm_name + '_hyperparameters' + '.csv'):
+        raise FileNotFoundError(f'File {algorithm_name}_hyperparameters.csv doesn\'t exist')
     with open('hyperparameters/' + algorithm_name + '_hyperparameters' + '.csv', mode='r') as csv_file:
         reader = csv.reader(csv_file)
         next(reader)
         for row in reader:
             env_name, param_key, param_value = row
-            print(env_name,environment_name)
             if env_name == environment_name:
+                try:
+                    if '.' in param_value:  # Sprawdzamy, czy zawiera kropkę
+                        param_value = float(param_value)
+                    else:
+                        param_value = int(param_value)
+                except ValueError:
+                    pass
+
                 hyperparameters[param_key] = param_value
-                print(11111)
-        print(hyperparameters)
+
         time_steps = int(hyperparameters.pop("total_timesteps"))
-
         n_envs = int(hyperparameters.pop("n_envs"))
-
     return hyperparameters, time_steps, n_envs
 
 
 def learn_model(environment_name, parameters, rl_algorithm):
     hyperparameters, time_steps, n_envs = parameters
     vec_env = make_vec_env(environment_name, n_envs=n_envs)
-    model = rl_algorithm(env=vec_env, verbose=1, **hyperparameters)
-
+    model = rl_algorithm(policy="MlpPolicy",env=vec_env, verbose=1, **hyperparameters)
     model.learn(total_timesteps=time_steps)  # Uczenie modelu
-    model.save('models/'+str(rl_algorithm.__name__)+'/' + environment_name + "_model")  # Zapisanie modelu
+    model.save('models/' + str(rl_algorithm.__name__) + '/' + str(rl_algorithm.__name__) + '_' + environment_name + "_model")  # Zapisanie modelu
     vec_env.close()
 
 
@@ -60,11 +61,23 @@ def create_model(environment_name, model_name):
         learn_model(environment_name, load_hyperparameters(environment_name, "DQN"), DQN)
 
 
+def return_model(model_name):
+    if model_name == "PPO":
+        return PPO
+    elif model_name == "DDPG":
+        return DDPG
+    elif model_name == "A2C":
+        return A2C
+    elif model_name == "DQN":
+        return DQN
+
+
 def test_models(environment_name, model_name):
-    model = PPO.load('models/' + model_name + '/' + environment_name + "_model")
+    algorithm = return_model(model_name)
+    model = algorithm.load('models/' + model_name + '/' + model_name + '_' + environment_name + '_model.zip')
     env = gym.make(environment_name, render_mode="human")
     obs, info = env.reset()
-    for _ in range(10000):
+    for _ in range(50):
         action, _ = model.predict(obs, deterministic=True)
         obs, reward, terminated, truncated, info = env.step(action)
         if terminated or truncated:
@@ -74,15 +87,13 @@ def test_models(environment_name, model_name):
 
 
 def main():
+    processes = []
     for environment_name in environments:
         for model in model_names:
-            if not os.path.exists('models/' + model + '/' + environment_name + '_model.zip'):
+            if not os.path.exists('models/' + model + '/' + model + '_' + environment_name + '_model.zip'):
                 create_model(environment_name, model)
             else:
                 test_models(environment_name, model)
-    params = load_hyperparameters("Swimmer-v4", "PPO")
-    print(params)
-
     pass
 
 
